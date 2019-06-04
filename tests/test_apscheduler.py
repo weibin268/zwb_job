041fb94dce0a3b565a912f135b4d05aad1f3be5b
zwb <zhuangweibin@stbl.cc>
2019-06-04 15:58:17 +0800

    aa

--- tests/test_apscheduler.py
+++ tests/test_apscheduler.py
@@ -8,8 +8,13 @@ def job1():
 
 scheduler = BackgroundScheduler()
 scheduler.add_job(job1, "interval", seconds=1)
-
 scheduler.start()
+
+
+@scheduler.scheduled_job("cron", second="*/1", hour="*")
+def job2():
+    print("2222")
+
 
 while True:
     time.sleep(10)
