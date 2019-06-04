2375e276c9a5516968a6d44cc1c5dec7088736d4
zwb <zhuangweibin@stbl.cc>
2019-06-04 15:28:09 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zwb_job",
+    version="1.0.2",
+    keywords=("util"),
+    description="zwb_job",
+    long_description="zwb_job",
+    url="https://github.com/weibin268/zwb_job",
+    author="zhuang weibin",
+    author_email="448075543@qq.com",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
