81cdeb939d3591b8272d16050a51f63b927a8e78
weibin268 <binweizhuang@hotmail.com>
2019-06-05 01:17:45 +0800

    aa

--- setup.py
+++ setup.py
@@ -12,5 +12,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     platforms="any",
-    install_requires=[]
+    install_requires=['APScheduler==3.6.0']
 )
---
+++ zwb_job/apscheduler_utils.py
@@ -0,0 +1,25 @@
+from apscheduler.schedulers.background import BackgroundScheduler, BlockingScheduler
+import time
+
+
+def get_scheduler(blocking=True):
+    scheduler = None
+    if blocking:
+        scheduler = BlockingScheduler()
+    else:
+        scheduler = BackgroundScheduler()
+    return scheduler
+
+
+if __name__ == "__main__":
+    def job1():
+        print("job1")
+
+    scheduler = get_scheduler()
+    scheduler.add_job(job1, "interval", seconds=1)
+
+    @scheduler.scheduled_job("cron", second="0/3", hour="*")
+    def job2():
+        print("job2")
+
+    scheduler.start()
