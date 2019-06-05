8b50bcc3fe92805a5e06b87d809e8f35e5bdde7a
zwb <zhuangweibin@stbl.cc>
2019-06-05 11:08:52 +0800

    aa

--- zwb_job/apscheduler_utils.py
+++ zwb_job/apscheduler_utils.py
@@ -2,7 +2,7 @@ from apscheduler.schedulers.background import BackgroundScheduler, BlockingSched
 import time
 
 
-def get_scheduler(blocking=True):
+def get_scheduler(blocking=False):
     scheduler = None
     if blocking:
         scheduler = BlockingScheduler()
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     def job1():
         print("job1")
 
-    scheduler = get_scheduler()
+    scheduler = get_scheduler(blocking=True)
     scheduler.add_job(job1, "interval", seconds=1)
 
     @scheduler.scheduled_job("cron", second="0/3", hour="*")
